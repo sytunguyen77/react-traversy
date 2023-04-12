8e44c7fd81d8494b2da71b78f3fed6508f12f5d2
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-04-12 17:28:45 -0500

    Update Task.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
+   const [showAddTask, setShowAddTask] = useState(true);
    const [tasks, setTasks] = useState([
       {
          id: 1,
@@ -24,10 +26,32 @@ const App = () => {
       },
    ]);
 
+   // Add Task
+   const addTask = (task) => {
+      const id = Math.floor(Math.random() * 10000) + 1;
+      const newTask = { id, ...task };
+      setTasks([...tasks, newTask]);
+   };
+
+   // Delete Task
+   const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id));
+   };
+
+   // Toggle Reminder
+   const toggleReminder = (id) => {
+      setTasks(tasks.map((task) => (task.id === id ? { ...task, reminder: !task.reminder } : task)));
+   };
+
    return (
       <div className="container">
-         <Header />
-         <Tasks tasks={tasks} />
+         <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+         {showAddTask && <AddTask onAdd={addTask} />}
+         {tasks.length > 0 ? (
+            <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+         ) : (
+            "No Tasks To Show"
+         )}
       </div>
    );
 };
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,15 @@
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
    return (
-      <div className="task">
+      <div className={`task ${task.reminder ? "reminder" : ""}`} onDoubleClick={() => onToggle(task.id)}>
          <h3>
-            {task.text} <FaTimes />
+<<<<<<< HEAD
+            {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} />
+=======
+            {task.text}{" "}
+            <FaTimes style={{ color: "red", cursor: "pointer" }} onClick={() => onDelete(task.id)} />
+>>>>>>> 70eac94126fefc75822dd74db5fd3c2f494a46ba
          </h3>
          <p>{task.day}</p>
       </div>
